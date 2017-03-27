38c238b3c6ddfd4e0f1d8c4ee9408c724eee7b1e
Tjelvar Olsson <tjelvar.olsson@jic.ac.uk>
2017-03-27 14:11:50 +0100

    Fix flake8 issues

--- setup.py
+++ setup.py
@@ -3,11 +3,12 @@ from setuptools import setup
 url = ""
 version = "0.1.0"
 readme = open('README.rst').read()
+dsc = "Python tools to manage users/groups/quotas/namespaces in an irods zone",
 
 setup(name="jicirodsmanager",
       packages=["jicirodsmanager"],
       version=version,
-      description="Python tools to manage users/groups/quotas/namespaces in an irods zone",
+      description=dsc,
       long_description=readme,
       include_package_data=True,
       author="Tjelvar Olsson",
