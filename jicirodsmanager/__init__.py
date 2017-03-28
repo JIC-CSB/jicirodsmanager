595253195b78fb58d7fe09e380e7dd2d6a4530b2
Tjelvar Olsson <tjelvar.olsson@jic.ac.uk>
2017-03-28 14:23:43 +0100

    Add StorageManager base class

--- jicirodsmanager/__init__.py
+++ jicirodsmanager/__init__.py
@@ -1,3 +1,48 @@
 """jicirodsmanager package."""
 
 __version__ = "0.1.0"
+
+
+class StorageManager(object):
+    """Class for adding users/groups/namespaces/quotas to a storage system."""
+
+# Class variable and methods that need to be overridden by subclasses.
+
+    command_prefix = "default"
+
+    def group_exists(self, group_name):
+        """Return true if the group exists."""
+
+    def create_group_without_quota(self, group_name):
+        """Add the group without setting a quota."""
+
+    def create_group_with_quota(self, group_name, quota):
+        """Add the group and set quota."""
+
+    def create_user(self, user_name):
+        """Create the user and return True if successful."""
+
+    def add_user_to_group(self, user_name, group_name):
+        """Create the user."""
+
+
+# Command line interface API.
+
+    def add_user(self, user_name, group_name):
+        """Add the user to the storage system."""
+        if not self.group_exists(group_name):
+            print("The group {} does not exist.".format(group_name))
+            print("Please create it using the {}-groupadd command.".format(
+                self.command_prefix))
+            return
+
+        # At this point the group should exist.
+        # If we manage to create the user we add it to the group.
+        if self.create_user(user_name):
+            self.add_user_to_group(user_name, group_name)
+
+    def add_group(self, group_name, quota=None):
+        """Add the group to the storage system."""
+        if quota is None:
+            return self.create_group_without_quota(group_name)
+        return self.create_group_with_quota(group_name, quota)
---
+++ tests/test_StorageManager_class.py
@@ -0,0 +1,103 @@
+"""Test the storage manager class."""
+
+try:
+    from unittest.mock import MagicMock
+except ImportError:
+    from mock import MagicMock
+
+
+def test_initialisation():
+
+    from jicirodsmanager import StorageManager
+    storage_manager = StorageManager()
+
+    # These are the public methods.
+    assert callable(storage_manager.group_exists)
+    assert callable(storage_manager.add_user)
+    assert callable(storage_manager.add_group)
+
+
+def test_add_user():
+    from jicirodsmanager import StorageManager
+    storage_manager = StorageManager()
+
+    # Mock stuff.
+    storage_manager.group_exists = MagicMock(return_value=True)
+    storage_manager.create_user = MagicMock(return_value=True)
+    storage_manager.add_user_to_group = MagicMock()
+
+    # Run command.
+    storage_manager.add_user("olssont", "rg-matthew-hartley")
+
+    # Assert stuff.
+    storage_manager.group_exists.assert_called_once_with("rg-matthew-hartley")
+    storage_manager.create_user.assert_called_once_with("olssont")
+    storage_manager.add_user_to_group.assert_called_once_with("olssont", "rg-matthew-hartley")
+
+
+def test_add_user_when_group_does_not_exist():
+    from jicirodsmanager import StorageManager
+    storage_manager = StorageManager()
+
+    # Mock stuff.
+    storage_manager.group_exists = MagicMock(return_value=False)
+    storage_manager.create_user = MagicMock(return_value=True)
+    storage_manager.add_user_to_group = MagicMock()
+
+    # Run command.
+    storage_manager.add_user("olssont", "rg-matthew-hartley")
+
+    # Assert stuff.
+    storage_manager.group_exists.assert_called_once_with("rg-matthew-hartley")
+    storage_manager.create_user.assert_not_called()
+    storage_manager.add_user_to_group.assert_not_called()
+
+
+def test_only_try_to_add_user_to_group_if_user_creation_was_successful():
+    from jicirodsmanager import StorageManager
+    storage_manager = StorageManager()
+
+    # Mock stuff.
+    storage_manager.group_exists = MagicMock(return_value=True)
+    storage_manager.create_user = MagicMock(return_value=False)
+    storage_manager.add_user_to_group = MagicMock()
+
+    # Run command.
+    storage_manager.add_user("olssont", "rg-matthew-hartley")
+
+    # Assert stuff.
+    storage_manager.group_exists.assert_called_once_with("rg-matthew-hartley")
+    storage_manager.create_user.assert_called_once_with("olssont")
+    storage_manager.add_user_to_group.assert_not_called()
+
+
+def test_add_group_no_quota():
+    from jicirodsmanager import StorageManager
+    storage_manager = StorageManager()
+
+    # Mock stuff.
+    storage_manager.create_group_with_quota = MagicMock()
+    storage_manager.create_group_without_quota = MagicMock()
+
+    # Run command.
+    storage_manager.add_group("rg-matthew-hartley")
+
+    # Assert stuff.
+    storage_manager.create_group_without_quota.assert_called_once_with("rg-matthew-hartley")
+    storage_manager.create_group_with_quota.assert_not_called()
+
+
+def test_add_group_with_quota():
+    from jicirodsmanager import StorageManager
+    storage_manager = StorageManager()
+
+    # Mock stuff.
+    storage_manager.create_group_with_quota = MagicMock()
+    storage_manager.create_group_without_quota = MagicMock()
+
+    # Run command.
+    storage_manager.add_group("rg-matthew-hartley", 500)
+
+    # Assert stuff.
+    storage_manager.create_group_without_quota.assert_not_called()
+    storage_manager.create_group_with_quota.assert_called_once_with("rg-matthew-hartley", 500)
