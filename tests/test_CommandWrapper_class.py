71ad43d9d8aabee8eda51391b5e186e22ad26420
Tjelvar Olsson <tjelvar.olsson@jic.ac.uk>
2017-03-29 21:50:30 +0100

    Simplified the CommandWrapper class

--- tests/test_CommandWrapper_class.py
+++ tests/test_CommandWrapper_class.py
@@ -10,56 +10,40 @@ import pytest
 
 def test_initialisation():
     from jicirodsmanager import CommandWrapper
-    cmd = CommandWrapper()
+    cmd = CommandWrapper([])
     assert callable(cmd)
 
 
 def test_cmd_when_successful():
     from jicirodsmanager import CommandWrapper
-    cmd = CommandWrapper()
+    cmd = CommandWrapper(["ls"])
 
     # Mock stuff.
-    cmd.cmd = ["ls"]
     cmd.success = MagicMock(return_value=True)
-    cmd.process_stdout = MagicMock()
-    cmd.process_stderr = MagicMock()
 
     # Run stuff.
     cmd()
 
     # Assert stuff.
     cmd.success.assert_called_once_with()
-    cmd.process_stdout.assert_called_once_with()
-    cmd.process_stderr.assert_not_called()
 
 
 def test_cmd_when_not_successful():
     from jicirodsmanager import CommandWrapper
-    cmd = CommandWrapper()
+    cmd = CommandWrapper(["ls"])
 
     # Mock stuff.
-    cmd.cmd = ["ls"]
     cmd.success = MagicMock(return_value=False)
-    cmd.process_stdout = MagicMock()
-    cmd.process_stderr = MagicMock()
 
     # Run stuff. Here we need to catch and ignore the RuntimeError
     # generated as a consequence of cmd.success() returning False.
-    try:
+    with pytest.raises(RuntimeError):
         cmd()
-    except RuntimeError:
-        pass
-
-    # Assert stuff.
-    cmd.success.assert_called_once_with()
-    cmd.process_stdout.assert_not_called()
-    cmd.process_stderr.assert_called_once_with()
 
 
 def test_cmd_raises_when_invoked_with_command_not_in_path():
     from jicirodsmanager import CommandWrapper
-    cmd = CommandWrapper()
-    cmd.cmd = ["rubbish"]
+    cmd = CommandWrapper(["rubbish"])
 
     # Run stuff.
     with pytest.raises(RuntimeError):
