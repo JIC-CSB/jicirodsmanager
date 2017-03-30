01131c84429bf4b1f28c230940d12627a0fe9855
Matthew Hartley <mhartley@cantab.net>
2017-03-30 11:42:46 +0100

    Log arguments to Popen before call in jicirodsmanager

--- jicirodsmanager/__init__.py
+++ jicirodsmanager/__init__.py
@@ -24,6 +24,7 @@ class CommandWrapper(object):
     def _call_cmd_line(self):
         """Run the command line tool."""
         try:
+            logging.info("Calling Popen with: {}".format(self.args))
             p = Popen(self.args, stdout=PIPE, stderr=PIPE)
         except OSError:
             raise(RuntimeError("No such command found in PATH"))
